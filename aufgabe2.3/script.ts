c3bc9c9f15943be2e1cf12a738f6143e8dc96e01
benjamin-maier <80950520+benjamin-maier@users.noreply.github.com>
2021-05-05 16:05:25 +0200

    Aufgabe 2.3 angefangen

---
+++ aufgabe2.3/script.ts
@@ -0,0 +1,57 @@
+namespace Aufgabe2_3{
+
+console.log("---------------");
+console.log("AUFGABE 1:");
+
+let y: number = 3;
+
+document.addEventListener("click", addRect);
+function addRect(event1: Event): void {
+    y = 1;
+    console.log("Button 1 wurde geklickt!");
+    for (let i = 0; i < y; i++) {
+        let div: HTMLDivElement = document.createElement("div");
+        div.style.backgroundColor = "black";
+        //let height1: number = Math.random() * 50;
+        div.style.margin = "10px";
+        div.style.height = "50px";
+        div.style.width = "500px";
+        document.body.appendChild(div);
+        y+1;
+    }
+}
+
+for (let i = 0; i < y; i++) {
+    let div: HTMLDivElement = document.createElement("div");
+    div.style.backgroundColor = "black";
+    //let height1: number = Math.random() * 50;
+    div.style.margin = "10px";
+    div.style.height = "50px";
+    div.style.width = "500px";
+    document.body.appendChild(div);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
