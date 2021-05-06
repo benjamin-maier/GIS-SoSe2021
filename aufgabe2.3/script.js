e3329212b2d1333a02b8b28ec107195869c8f7b8
benjamin-maier <80950520+benjamin-maier@users.noreply.github.com>
2021-05-06 21:28:11 +0200

    Aufgabe 1 gemacht

--- aufgabe2.3/script.js
+++ aufgabe2.3/script.js
@@ -3,29 +3,62 @@ var Aufgabe2_3;
 (function (Aufgabe2_3) {
     console.log("---------------");
     console.log("AUFGABE 1:");
-    let y = 3;
+    //let y: number = 3;
     /*document.addEventListener("click", addRect);
     function addRect(event1: Event): void {
         y = 1;
         console.log("Button 1 wurde geklickt!");*/
-    for (let i = 0; i < y; i++) {
-        let div = document.createElement("div");
+    /*for (let i = 0; i < y; i++) {
+        let div: HTMLDivElement = document.createElement("div");
         div.style.backgroundColor = "black";
         //let height1: number = Math.random() * 50;
         div.style.margin = "10px";
         div.style.height = "50px";
         div.style.width = "500px";
         document.body.appendChild(div);
-        y + 1;
+        y+1;
     }
-    for (let i = 0; i < y; i++) {
+
+for (let i = 0; i < y; i++) {
+    let div: HTMLDivElement = document.createElement("div");
+    div.style.backgroundColor = "black";
+    //let height1: number = Math.random() * 50;
+    div.style.margin = "10px";
+    div.style.height = "50px";
+    div.style.width = "500px";
+    document.body.appendChild(div);
+}*/
+    let addRecButton = document.createElement("Button");
+    let textRecButton = document.createTextNode("add Rectangle");
+    addRecButton.appendChild(textRecButton);
+    document.body.appendChild(addRecButton);
+    addRecButton.id = "RecButtId";
+    let resetWindowButton = document.createElement("Button");
+    let textWinButton = document.createTextNode("Clear");
+    resetWindowButton.id = "resetButtId";
+    resetWindowButton.appendChild(textWinButton);
+    document.body.appendChild(resetWindowButton);
+    document.getElementById("RecButtId").addEventListener("click", createDivRec);
+    document.getElementById("resetButtId").addEventListener("click", clearPage);
+    for (let i = 0; i < 3; i++) {
+        createDivRec();
+    }
+    function clearPage() {
+        window.location.reload();
+    }
+    function createDivRec() {
+        let colors = ["grey", "red", "brown", "black"];
+        let previousElement = document.body;
         let div = document.createElement("div");
-        div.style.backgroundColor = "black";
-        //let height1: number = Math.random() * 50;
-        div.style.margin = "10px";
-        div.style.height = "50px";
-        div.style.width = "500px";
-        document.body.appendChild(div);
+        div.style.height = (Math.random() * 400).toString() + "px";
+        div.style.width = (Math.random() * 400).toString() + "px";
+        div.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)].toString();
+        div.style.position = "absolute";
+        div.style.left = (Math.random() * 400).toString() + "px";
+        div.style.right = (Math.random() * 400).toString() + "px";
+        div.classList.add("rectangle");
+        previousElement.appendChild(div);
+        previousElement = div;
     }
 })(Aufgabe2_3 || (Aufgabe2_3 = {}));
 //# sourceMappingURL=script.js.map
--- aufgabe2.3/script.ts
+++ aufgabe2.3/script.ts
@@ -3,13 +3,13 @@ namespace Aufgabe2_3{
 console.log("---------------");
 console.log("AUFGABE 1:");
 
-let y: number = 3;
+//let y: number = 3;
 
 /*document.addEventListener("click", addRect);
 function addRect(event1: Event): void {
     y = 1;
     console.log("Button 1 wurde geklickt!");*/
-    for (let i = 0; i < y; i++) {
+    /*for (let i = 0; i < y; i++) {
         let div: HTMLDivElement = document.createElement("div");
         div.style.backgroundColor = "black";
         //let height1: number = Math.random() * 50;
@@ -28,5 +28,46 @@ for (let i = 0; i < y; i++) {
     div.style.height = "50px";
     div.style.width = "500px";
     document.body.appendChild(div);
-}
+}*/
+let addRecButton: HTMLElement = document.createElement("Button");
+    let textRecButton: Text = document.createTextNode("add Rectangle");
+    addRecButton.appendChild(textRecButton);
+    document.body.appendChild(addRecButton);
+    addRecButton.id = "RecButtId";
+
+    let resetWindowButton: HTMLElement = document.createElement("Button");
+    let textWinButton: Text = document.createTextNode("Clear");
+    resetWindowButton.id = "resetButtId";
+
+    resetWindowButton.appendChild(textWinButton);
+    document.body.appendChild(resetWindowButton);
+
+    document.getElementById("RecButtId").addEventListener("click", createDivRec);
+    document.getElementById("resetButtId").addEventListener("click", clearPage);
+
+    for (let i = 0; i < 3; i++) {
+        createDivRec();
+    }
+
+    function clearPage(): void {
+        window.location.reload();
+    }
+
+    function createDivRec(): void {
+
+        let colors: String[] = ["grey", "red", "brown", "black"];
+        let previousElement: HTMLElement = document.body;
+        
+        let div: HTMLDivElement = document.createElement("div");
+        div.style.height = (Math.random() * 400).toString() + "px";
+        div.style.width = (Math.random() * 400).toString() + "px";
+        div.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)].toString();
+        div.style.position = "absolute";
+        div.style.left = (Math.random() * 400).toString() + "px";
+        div.style.right = (Math.random() * 400).toString() + "px";
+        div.classList.add("rectangle");
+        previousElement.appendChild(div);
+        previousElement = div;
+
+    }
 }
